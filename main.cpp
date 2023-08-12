490af43bc0a3ac398ce67d061e33a26e483b52ba
Erik Bjur <erik@bjurengineering.com>
2023-08-11 18:09:41 -0700

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello class" << std::endl;
+
+    std::cout << "Hit enter to count to 10" << std::endl;
+    std::cin.get();
+
+    for( int i = 1; i < 11; i++) {
+        std::cout << i << std::endl;
+    }
+}
