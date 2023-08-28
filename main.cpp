61e5c49daa320de97e812ad65f70d31d3a21eba2
Erik Bjur <erik@bjurengineering.com>
2023-08-27 18:45:51 -0700

    Changed end number to 1000

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 int main() {
     std::cout << "Hello class" << std::endl;
 
-    std::cout << "Hit enter to count to 10" << std::endl;
+    std::cout << "Hit enter to count to 1000" << std::endl;
     std::cin.get();
 
-    for( int i = 1; i < 10+1; i++) {
+    for( int i = 1; i < 1000+1; i++) {
         std::cout << i << std::endl;
     }
 }
