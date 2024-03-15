61d77faae3dafd639f6e85c7dda2cc076ab70751
Lucas Lange <lucasjlange104@gmail.com>
2024-03-14 23:19:23 -0700

    oops I guess It cant count to 10,000 made it count only to 1001

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 int main() {
     std::cout << "Hello class" << std::endl;
 
-    std::cout << "Hit enter to count to 10000" << std::endl;
+    std::cout << "Hit enter to count to 1001" << std::endl;
     std::cin.get();
 
-    for( int i = 0; i < 10000; i++) {
+    for( int i = 1; i < 1001 + 1; i++) {
         std::cout << i << std::endl;
     }
 }
