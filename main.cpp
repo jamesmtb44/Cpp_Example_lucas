50801167aa130ec9f2b35d4186085d6f055aae0e
Lucas Lange <lucasjlange104@gmail.com>
2024-03-14 23:15:18 -0700

    1000!? how about 10000

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 int main() {
     std::cout << "Hello class" << std::endl;
 
-    std::cout << "Hit enter to count to 1000" << std::endl;
+    std::cout << "Hit enter to count to 10000" << std::endl;
     std::cin.get();
 
-    for( int i = 1; i < 1000+1; i++) {
+    for( int i = 0; i < 10000; i++) {
         std::cout << i << std::endl;
     }
 }
